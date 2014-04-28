2673ad76800ffc1b3543dbe30dd1f5995d80fd3e
Dan Hahn <dhahn@appnexus.com>
2014-04-28 14:38:38 -0400

    Changing to .table

--- js/global.js
+++ js/global.js
@@ -1,24 +1,50 @@
 $(function() {
     
-    var btnList = [];
+    var btnList = ['all'];
     
+    // Build list of all items for the buttons
     var buildBtnLlist = function (element) {
         (_.indexOf(btnList, element) === -1) ? btnList.push(element) : "";        
     };
     
+    // Highlight all rows that match button
     var highlightRow = function() {
         var $this = $(this);
-        var rowType = "."+$this.data('action');
+        var btnType = $this.data('action');
+        var rowType = "."+btnType;
         $('.success').removeClass('success');
         $('.inactive').removeClass('inactive');
-        $(rowType).addClass('success');
-        $('tr:not('+rowType+')').addClass('inactive').slideUp("slow");
+        if(btnType !== "all") {
+            $(rowType).addClass('success');
+            $('tr:not('+rowType+')').addClass('inactive');
+        }
     };
+
+    var displayAttrData = function() {
+        var $el = $(this);
+        var data = $el.data('attr');
+        var $newRow = $('<tr><td colspan="3"></td></tr>');
+        var $attrTable = $('<section class="attr-content hide-height"><table class="table table-bordered">');
+        $.each(data, function(index, value) {
+            var compiled = _.template('<tr><td><div><code><%= attr %></code></div></td><td><%= value %></td><td><%= d %></tr>');
+            var $row = $(compiled(value));
+            $attrTable.find("table").append($row);
+        });
+        $newRow.find("td").append($attrTable);
+        $newRow.insertAfter($el);
+    }
     
+    // add value and class to rows that have attr data
+    var addAttrData = function(el, data) {
+        el.find("td div").addClass("attr");
+        el.data("attr", data);
+        el.on("click", displayAttrData);
+    }
     
+    // Build button group with all itme in Button List
     var displayBtn = function(btn) {
         
-        var $btnGroup = $('<div class="btn-group pull-right"><button type="buttom" class="btn btn-default">All</button></div>')
+        var $btnGroup = $('<div class="btn-group pull-right"></div>')
         
         $.each(btn, function(index, value) {
             var $btn = $('<button type="buttom" class="btn btn-default"></button>').html(value);
@@ -30,19 +56,19 @@ $(function() {
         $btnGroup.insertBefore("h1");
     }
     
+    // Loop thogth the item in object to build table
     var buildTable = function(data) {
         var $table = $("<table class='table table-bordered'>");
         var $headerRow = $("<tr><th>Name</th><th>Description</th><th>Type</th></tr>");
         $headerRow.appendTo($table);
         $.each(data, function(index, value){
             buildBtnLlist(value.type);
-            var compiled = _.template('<tr class="<%= type %>"><td><code>&lt;<%= tag %>&gt;</code></td><td><%= desc %></td><td><%= type %></tr>');
-            var output = compiled(value);
-            $table.append(output);        
-        });        
-        
+            var compiled = _.template('<tr class="<%= type %>"><td><div><code>&lt;<%= tag %>&gt;</code></div></td><td><%= desc %></td><td><%= type %></tr>');
+            var $row = $(compiled(value));
+            (value.attr) ? addAttrData($row, value.attr) : "";
+            $table.append($row);
+        });
         displayBtn(btnList);
-                
         $table.appendTo(".display");
     };
     
